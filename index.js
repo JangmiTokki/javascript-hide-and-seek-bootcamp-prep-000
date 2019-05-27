b7707cb11f3affe7e3c001a4c7f7a59d64618dcb
communal-principle-3181 <aellis19@outlook.com>
2019-05-27 01:21:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector)
+{
+  var selection = document.querySelector(selector);
+  return selection;
+}
 
+function nestedTarget()
+{
+  var target = document.querySelector("#nested .target");
+  return target;
+}
+
+function increaseRankBy(n)
+{ 
+  const rankUp = document.querySelectorAll("ul.ranked-list");
+  for (let i = 0; i < rankUp.length; i++)
+  {
+   rankUp[i].innerHTML = parseInt(rankUp[i].innerHTML) + n;
+  }
+  return rankUp;
+}
+
+function deepestChild()
+{
+  var parent= document.getElementById("grand-node").querySelectorAll("div");
+  var child = " ";
+for( let i = 0; i < parent.length; i++)
+{
+  child = parent[i].querySelector("div");
+}
+  return parent;
+}
