1f74fb99a512852478acb793b24fc531cf8e8dd1
communal-principle-3181 <aellis19@outlook.com>
2019-05-27 01:23:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector)
+{
+  var selection = document.querySelector(selector);
+  return selection;
+}
 
+function nestedTarget()
+{
+  var target = document.querySelector("#nested .target");
+  return target;
+}
+
+function increaseRankBy(n)
+{ 
+  const rankUp = document.querySelectorAll("ul.ranked-list");
+  for (let i = 0; i < rankUp.length; i++)
+  {
+   rankUp[i].innerHTML = parseInt(rankUp[i].innerHTML) + n;
+  }
+  return rankUp;
+}
+
+function deepestChild()
+{
+  var parent= document.getElementById("grand-node").querySelectorAll("div");
+
+  return parent[parent.length-1];
+}
